e20af05c8a835d41db81daa7c5123bde0702c03b
Marlos Bosso <marlosbosso@outlook.com>
2025-09-23 14:30:32 -0300

    create a new model module

--- 103 Train/b_model.py
+++ 103 Train/b_model.py
@@ -155,3 +155,13 @@ class DistilGPT2(nn.Module):
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)
         return logits
+    
+@dataclass
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 30000 
+    n_layer: int = 6
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.1
+    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
